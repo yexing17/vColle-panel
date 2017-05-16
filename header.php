4de96bb39506a8c94eaef585e20572b58f88effc
Fran Lin <linfran@163.com>
2017-05-16 15:53:08 +0800

    Initialization

---
+++ header.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <script src="https://cdn.bootcss.com/jquery/3.2.1/jquery.min.js"></script>
+    <script src="http://yx17-1252304265.cossh.myqcloud.com/cdn/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="http://yx17-1252304265.cossh.myqcloud.com/cdn/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="http://hk.hisun.me/vp/css/main.css">
+    <title>vCollection</title>
+</head>
+<body>
+<header class="site-header">
+    <nav class="navbar navbar-default" role="navigation">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse"
+                        data-target="#example-navbar-collapse">
+                    <span class="sr-only">Switch-Navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">vCollections</a>
+            </div>
+            <div class="collapse navbar-collapse" id="navbar-collapse">
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="#">Today</a></li>
+                    <li><a href="#">Feel-lucky</a></li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                            All-Subscriptions<b class="caret"></b>
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">Subscription1</a></li>
+                            <li><a href="#">Subscription2</a></li>
+                            <li><a href="#">Subscription3</a></li>
+                            <li class="divider"></li>
+                            <li><a href="#">Manage-Subscription</a></li>
+                        </ul>
+                    </li>
+                    <li class="dropdown pull">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                            All-Favorite <b class="caret"></b>
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">Default-Tag</a></li>
+                            <li><a href="#">Tag1</a></li>
+                            <li><a href="#">Tag2</a></li>
+                            <li class="divider"></li>
+                            <li><a href="#">Manage-Tags</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+            <div class="collapse navbar-collapse pull-right" id="navbar-usercenter">
+                <div class="btn-group">
+                    <button type="button" class="btn btn-primary">UserName</button>
+                    <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">
+                        <span class="caret"></span>
+                        <span class="sr-only">Switch the Drop-down Men</span>
+                    </button>
+                    <ul class="dropdown-menu" role="menu">
+                        <li><a href="#">User-Center</a></li>
+                        <li><a href="#">Account-Information</a></li>
+                        <li><a href="#">Setting</a></li>
+                        <li class="divider"></li>
+                        <li><a href="#">Logout</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </nav>
+</header>
+<div class="container">
